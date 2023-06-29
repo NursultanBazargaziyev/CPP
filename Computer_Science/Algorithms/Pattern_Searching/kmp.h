36fc85774596e08cdfed5069041ae60bf8b49162
Bazargaziyev Nursultan <nursultanbazargaziyev@gmail.com>
2023-06-29 15:43:07 +0200

    Implemented Quick-sort algorithm

---
+++ Computer_Science/Algorithms/Pattern_Searching/kmp.h
@@ -0,0 +1,20 @@
+#ifndef KNUTH_MORRIS_PRATT
+#define KNUTH_MORRIS_PRATT
+
+//  O(n * m) - KMP
+//  O(n) - substring search
+//  n - length of the text  
+//  m - length of the pattern
+
+#include <string>
+
+
+
+int kmp(std::string& text, std::string& pattern){
+
+}
+
+
+
+
+#endif
---
+++ Computer_Science/Algorithms/Sorting/QuickSort.h
@@ -0,0 +1,35 @@
+// Bazargaziyev Nursultan
+// 29-06-2023
+
+// Implementation of QuickSort;
+// Worst case: O(n^2);
+// Average/best case O(nlogn)
+
+template<typename T>
+void printArray(const std::vector& array<T> arr){
+    std::copy(arr.begin(),arr.end(), std::ostream_iterator<T>(cout, ", "));
+}
+
+template<typename T>
+void quickSort(std::vector<T>& arr, int l, int r){
+    if (l < r) {
+		int pi = partition(arr, l, r);
+		quickSort(arr, l, pi - 1);
+		quickSort(arr, pi + 1, r);
+	}
+}
+
+template<typename T>
+int partition(std::vector<T>& arr, int l, int r){
+    T pivot = arr[r];
+	int i = l, j = l - 1;
+	while (i < r) {
+		if (arr[i] < pivot) {
+			j++;
+			std::swap(arr[i], arr[j]);
+		}
+		i++;
+	}
+	std::swap(arr[j + 1], arr[r]);
+	return ++j;
+}
